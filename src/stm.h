ebf7a1fd94fb0ca69b709880d8ddaa7784ea332e
Jan Fiedor <fiedorjan@centrum.cz>
2014-06-19 17:35:15 +0200

    Reseted stm.h to the original version.

--- src/stm.h
+++ src/stm.h
@@ -19,7 +19,6 @@
 
 #include "tl2.h"
 #include "util.h"
-#include "eventlog.h"
 
 #define STM_THREAD_T                    Thread
 #define STM_SELF                        Self
@@ -47,13 +46,10 @@
 
 
 #define STM_VALID()                     (1)
-#define STM_RESTART()                   do { \
-                                            LOG_EVENT(TX_ABORT, *(long*)STM_SELF, __FILE__, __LINE__); \
-                                            TxAbort(STM_SELF); \
-                                        } while (0) /* enforce comma */
+#define STM_RESTART()                   TxAbort(STM_SELF)
 
 #define STM_STARTUP()                   TxOnce()
-#define STM_SHUTDOWN()                  TxShutdown(); printStatistics()
+#define STM_SHUTDOWN()                  TxShutdown()
 
 #define STM_NEW_THREAD()                TxNewThread()
 #define STM_INIT_THREAD(t, id)          TxInitThread(t, id)
@@ -70,46 +66,28 @@
                                             STM_JMPBUF_T STM_JMPBUF; \
                                             int STM_RO_FLAG = isReadOnly; \
                                             sigsetjmp(STM_JMPBUF, 1); \
-                                            LOG_EVENT(TX_START, *(long*)STM_SELF, __FILE__, __LINE__); \
                                             TxStart(STM_SELF, &STM_JMPBUF, &STM_RO_FLAG); \
                                         } while (0) /* enforce comma */
 
 #define STM_BEGIN_RD()                  STM_BEGIN(1)
 #define STM_BEGIN_WR()                  STM_BEGIN(0)
-#define STM_END()                       do { \
-                                            if (TxCommit(STM_SELF)) \
-                                              LOG_EVENT(TX_COMMIT, *(long*)STM_SELF, __FILE__, __LINE__); \
-                                            else \
-                                              LOG_EVENT(TX_ABORT, *(long*)STM_SELF, __FILE__, __LINE__); \
-                                        } while (0) /* enforce comma */
+#define STM_END()                       TxCommit(STM_SELF)
 
 typedef volatile intptr_t               vintp;
 
-static __attribute__((always_inline)) intptr_t TxReadWrapper(Thread* Self, volatile intptr_t* Addr)
-{
-  LOG_EVENT(TX_READ, *(long*)STM_SELF, __FILE__, __LINE__);
-  return TxLoad(Self, Addr);
-}
-
-static __attribute__((always_inline)) void TxWriteWrapper(Thread* Self, volatile intptr_t* addr, intptr_t valu)
-{
-  LOG_EVENT(TX_WRITE, *(long*)STM_SELF, __FILE__, __LINE__);
-  TxStore(Self, addr, valu);
-}
-
-#define STM_READ(var)                   TxReadWrapper(STM_SELF, (vintp*)(void*)&(var))
-#define STM_READ_F(var)                 IP2F(TxReadWrapper(STM_SELF, \
+#define STM_READ(var)                   TxLoad(STM_SELF, (vintp*)(void*)&(var))
+#define STM_READ_F(var)                 IP2F(TxLoad(STM_SELF, \
                                                     (vintp*)FP2IPP(&(var))))
-#define STM_READ_P(var)                 IP2VP(TxReadWrapper(STM_SELF, \
+#define STM_READ_P(var)                 IP2VP(TxLoad(STM_SELF, \
                                                      (vintp*)(void*)&(var)))
 
-#define STM_WRITE(var, val)             TxWriteWrapper(STM_SELF, \
+#define STM_WRITE(var, val)             TxStore(STM_SELF, \
                                                 (vintp*)(void*)&(var), \
                                                 (intptr_t)(val))
-#define STM_WRITE_F(var, val)           TxWriteWrapper(STM_SELF, \
+#define STM_WRITE_F(var, val)           TxStore(STM_SELF, \
                                                 (vintp*)FP2IPP(&(var)), \
                                                 F2IP(val))
-#define STM_WRITE_P(var, val)           TxWriteWrapper(STM_SELF, \
+#define STM_WRITE_P(var, val)           TxStore(STM_SELF, \
                                                 (vintp*)(void*)&(var), \
                                                 VP2IP(val))
 
